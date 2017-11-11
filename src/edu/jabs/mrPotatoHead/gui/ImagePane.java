d756bd87d05d6cb070182044b3c20f9cfa6b1cea
Joan Andrés <andres6936@live.com>
2017-11-11 00:01:58 -0500

    Modify ImagePane.java

--- src/edu/jabs/mrPotatoHead/gui/ImagePane.java
+++ src/edu/jabs/mrPotatoHead/gui/ImagePane.java
@@ -1,5 +1,31 @@
 package edu.jabs.mrPotatoHead.gui;
 
-public class ImagePane
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+
+/**
+ * Class where the header image is placed
+ */
+public class ImagePane extends JPanel
 {
+
+    /**
+     * Default constructor method.  It puts the header image of the application
+     */
+    public ImagePane( )
+    {
+        JLabel image = new JLabel( );
+        ImageIcon icon = new ImageIcon( "data/images/title.png" );
+        // It's added to the label
+        image = new JLabel( "" );
+        image.setIcon( icon );
+        add( image );
+
+        setBackground( Color.WHITE );
+        setBorder( new LineBorder( Color.BLACK ) );
+    }
 }
